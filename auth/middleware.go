04a81ae5f9f9e2d99baeca3f26d22404d33b914d
renosyah <syahputrareno975@gmail.com>
2020-07-09 21:36:30 +0700

    add allow header CORS policy

--- auth/middleware.go
+++ auth/middleware.go
@@ -7,6 +7,9 @@ import (
 func AuthenticationMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		
 		// auth session
 
 		next.ServeHTTP(w, r)

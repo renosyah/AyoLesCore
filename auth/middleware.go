1510ae23b0434b404806d03c8a9a0364ca408d97
renosyah <syahputrareno975@gmail.com>
2020-07-12 14:39:46 +0700

    add header setting

--- auth/middleware.go
+++ auth/middleware.go
@@ -7,9 +7,10 @@ import (
 func AuthenticationMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-		
+		w.Header().Add("Access-Control-Allow-Origin", "*")
+		w.Header().Add("Access-Control-Allow-Methods", "POST")
+		w.Header().Add("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With")
+
 		// auth session
 
 		next.ServeHTTP(w, r)
